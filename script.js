4aaf1c330db40b2a0a4f7285f418edff9c55cf42
azimuddin <azimnm99@gmail.com>
2025-10-03 18:37:00 +0530

    added delete button

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-const toDoTasks = [];
+const toDoTasks = [{name:'fresh hona', dueDate:'2025-10-5'},
+                   {name:'brush karna', dueDate:'2025-10-5'}];
 
 const toDoInput = document.querySelector('.task-input');
-
+const toDoDueDate = document.querySelector('.task-due-date');
 
 function addingTasks(){
   const toDoValue = toDoInput.value;
-  toDoTasks.push(toDoValue);
+  const dueDate = toDoDueDate.value;
+  toDoTasks.push({name: toDoValue, dueDate: dueDate});
   console.log(toDoTasks);
   toDoInput.value = '';
+  showingToDoTasks();
+}
+
+showingToDoTasks();
+
+function showingToDoTasks(){
+  let ToDoTasksList = ``;
+
+  for(let index = 0; index < toDoTasks.length; index++){
+    let todoObject = toDoTasks[index];
+    let name = todoObject.name;
+    let dueDate = todoObject.dueDate;
+    let todoHtml = `<p>
+                        ${name} ${dueDate}
+                        <button class="delete-task-btn" onclick="btnDeleteTask('${todoObject}')">delete
+                        </button>
+                    </p>`;
+    ToDoTasksList = ToDoTasksList + todoHtml;
+  }
+  document.querySelector('.task-list').innerHTML = ToDoTasksList;
+}
+
+
+
+function btnDeleteTask(task){
+  toDoTasks.splice(task, 1);
+  showingToDoTasks();
 }
